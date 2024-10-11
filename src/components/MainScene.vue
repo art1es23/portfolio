ab3882a5f51c65e29c7ff87efa18a8c4840ac8de
qmpwwsd <dreamwinamounts@gmail.com>
2024-10-12 01:24:40 +0300

    init 0.0.0

---
+++ src/components/MainScene.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="scene" class="m-auto scene">CanvasScene</div>
+</template>
+
+<script>
+export default {
+  name: 'MainScene',
+}
+</script>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import * as PIXI from 'pixi.js'
+
+const scene = ref(null)
+const app = ref(null)
+
+onMounted(() => {
+  app.value = new PIXI.Application({
+    width: 800,
+    height: 600,
+    backgroundColor: 0x1099bb,
+  })
+})
+</script>
